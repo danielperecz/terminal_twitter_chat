7e922a6900c80f7f3c7554b59538329cbd5b98ad
pereczd2 <daniel.perecz2@mail.dcu.ie>
2019-07-28 20:59:53 +0100

    Sending messages is now implemented

--- src/chat.py
+++ src/chat.py
@@ -1,7 +1,7 @@
 from logging.config import dictConfig
 from platform import system
 import logging
-import twitter
+import tweepy
 import yaml
 import sys
 import os
@@ -18,6 +18,9 @@ class TwitterTerminalChat:
 
     logger = logging.getLogger("chat.py")
 
+    api = None
+    my_id = None
+
     def __init__(self):
         self.logger.info("Initialising TwitterTerminalChat()")
 
@@ -27,28 +30,63 @@ class TwitterTerminalChat:
             self.logger.error("Invalid account.txt")
             sys.exit()
 
+        # Initialise keys & tokens from text file
         self.consumer_key = account[0]
         self.consumer_secret = account[1]
         self.access_token_key = account[2]
         self.access_token_secret = account[3]
 
-        # Initialize an Api object
+        # Authenticate user, setup api, and verify whether successful
         try:
-            self.api = twitter.Api(self.consumer_key, self.consumer_secret,
-                                   self.access_token_key, self.access_token_secret)
-            self.api.VerifyCredentials()
+            self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+            self.auth.set_access_token(self.access_token_key, self.access_token_secret)
+            self.api = tweepy.API(self.auth)
+            self.api.verify_credentials()
         except:
+            # If unsuccessful verify_credentials() causes an exception
             self.logger.error("Invalid key(s) and or token(s) in account.txt")
             sys.exit()
 
+        self.my_id = self.api.verify_credentials()._json["id"]
         self.logger.info("Successfully initialised TwitterTerminalChat()")
 
     def read_account(self):
-        with open(os.path.abspath("..") + slash + "account.txt", "r") as f:
+        """Method reads account.txt and returns its contents as a list of lines."""
+        with open(os.path.abspath("..") + slash + "account_.txt", "r") as f:
             lines = f.read().splitlines()
             if len(lines) == 4:
                 return lines
         return False
 
+    def open_chat(self):
+        """This is the actual method responsible for chatting. User is prompted for a recipient username and then they
+        may start chatting."""
 
-TwitterTerminalChat()
+        # Prompt user to enter a recipient username
+        try:
+            recipient_id = self.api.get_user(screen_name=input("Open chat with: "))._json["id"]
+        except:
+            # User not found. Call open_chat() again to prompt user for new user
+            self.logger.error("User not found")
+            self.open_chat()
+
+        message = input(">>> ")
+
+        # Keep prompting user for new input until "exit" is entered
+        while message.lower() != "exit":
+            self.api.send_direct_message(recipient_id=recipient_id, text=message)       # Send message
+            message = input(">>> ")                                                     # Prompt user for new message
+
+        # "exit" has been entered therefore stop code execution
+        sys.exit()
+
+
+TwitterTerminalChat().open_chat()
+
+"""
+Notes for myself:
+listing direct messages is rate limited
+
+latest_message = self.api.list_direct_messages()[0].message_create["message_data"]["text"]
+sender_id = self.api.list_direct_messages()[0].message_create["sender_id"]
+"""
